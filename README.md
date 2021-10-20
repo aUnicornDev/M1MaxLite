9dacae3f4d89e0aabf65337f443eb549609aab95
aUnicornDev <86775297+aUnicornDev@users.noreply.github.com>
2021-10-20 20:20:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# M1MaxLite
+ Every website gets a NOTCH !!!
---
+++ contentScript.js
@@ -0,0 +1,8 @@
+
+console.log(document.getElementsByTagName('body')[0].innerHTML)
+document.getElementsByTagName('body')[0].innerHTML += `
+<div class="notch">
+<div class = "camera"></div>
+</div>
+`;
+
